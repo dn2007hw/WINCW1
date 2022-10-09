597991a7f07fdc7a8b3daf6fb58ea4833946957a
Dayanandan Natarajan <ndaya@outlook.com>
2022-10-09 21:07:13 +0100

    first

---
+++ CWBrowser01/CWBrowser01/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CWBrowser01
+{
+    public static class HomePage
+    {
+        public static string homePage;
+
+
+
+    }
+}
